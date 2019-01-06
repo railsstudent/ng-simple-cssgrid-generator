c308b8687d3707680a8e42fb1ef200395e651f6e
Connie Leung <cleung334@gmail.com>
2019-01-06 12:19:10 +0800

    chore: set up prettier configuration
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
-  title = 'ng-simple-cssgrid-generator';
+export class AppComponent implements OnInit {
+    title = 'Very Simple CSS Grid Generator';
+
+    constructor(private titleService: Title) {}
+
+    ngOnInit() {
+        this.titleService.setTitle('Simple css grid generator');
+    }
 }
