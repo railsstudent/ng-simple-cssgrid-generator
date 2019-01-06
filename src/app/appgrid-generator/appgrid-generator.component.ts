59dc43c593fce42a3f3433f4a3dff99e763f772c
Connie Leung <cleung334@gmail.com>
2019-01-06 13:14:51 +0800

    create logic to generate css grid code
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/appgrid-generator/appgrid-generator.component.ts
+++ src/app/appgrid-generator/appgrid-generator.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, Renderer, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-grid-generator',
@@ -16,13 +16,13 @@ export class AppGridGeneratorComponent implements OnInit {
       grid-template-rows: repeat(3, 1fr);
     }`;
 
-    constructor(private renderer: Renderer) {}
+    constructor(private renderer: Renderer2) {}
 
     ngOnInit() {
         console.log(this.grid);
 
-        this.renderer.setElementStyle(this.grid.nativeElement, 'grid-template-columns', 'repeat(3, 1fr)');
-        this.renderer.setElementStyle(this.grid.nativeElement, 'grid-template-rows', 'repeat(2, 1fr)');
-        this.renderer.setElementStyle(this.grid.nativeElement, 'grid-auto-flow', 'column');
+        this.renderer.setStyle(this.grid.nativeElement, 'grid-template-columns', 'repeat(3, 1fr)');
+        this.renderer.setStyle(this.grid.nativeElement, 'grid-template-rows', 'repeat(2, 1fr)');
+        this.renderer.setStyle(this.grid.nativeElement, 'grid-auto-flow', 'column');
     }
 }
