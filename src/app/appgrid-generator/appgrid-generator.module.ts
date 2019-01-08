7e0aa914c538f8e7e761b858ee317456ccdd44fc
Connie Leung <cleung334@gmail.com>
2019-01-09 01:08:55 +0800

    refactor form to custom component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/appgrid-generator/appgrid-generator.module.ts
+++ src/app/appgrid-generator/appgrid-generator.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppGeneratorFormModule } from '../appgrid-generator-form/appgrid-generator-form.module';
 import { ShareModule } from '../share.module';
 import { AppGridGeneratorComponent } from './appgrid-generator.component';
 
 @NgModule({
     declarations: [AppGridGeneratorComponent],
-    imports: [ShareModule, ReactiveFormsModule],
+    imports: [ShareModule, ReactiveFormsModule, AppGeneratorFormModule],
     exports: [AppGridGeneratorComponent],
 })
 export class AppGeneratorModule {}
