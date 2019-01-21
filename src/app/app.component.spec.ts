1be59e7c6b7d5135811b11968f8767670da9a11c
Connie Leung <cleung334@gmail.com>
2019-01-22 02:37:10 +0800

    fix: test cases app component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,35 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppGeneratorFormComponent } from './appgrid-generator-form/appgrid-generator-form.component';
+import { AppGridGeneratorComponent } from './appgrid-generator/appgrid-generator.component';
+import { ShareModule } from './share.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent, AppGridGeneratorComponent, AppGeneratorFormComponent],
+            imports: [ReactiveFormsModule, ShareModule, NoopAnimationsModule],
+        }).compileComponents();
+    }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'ng-simple-cssgrid-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-simple-cssgrid-generator');
-  });
+    it(`should have as title 'Very Simple CSS Grid Generator'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('Very Simple CSS Grid Generator');
+    });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-simple-cssgrid-generator!');
-  });
+    it('should render title in a h1 tag', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Very Simple CSS Grid Generator');
+    });
 });
