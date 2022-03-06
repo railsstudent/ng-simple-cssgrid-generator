3f6d00cf181ba08f51ed200b729b99b8775ec3de
Connie Leung <cleung334@gmail.com>
2022-03-06 22:15:30 +0800

    feat: appGridValueField component

--- src/app/appgrid-template-form/appgrid-template-form.module.ts
+++ src/app/appgrid-template-form/appgrid-template-form.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
+import { AppGridValueFieldComponent } from '../appgrid-value-field'
 import { ShareModule } from '../share.module'
 import { AppTemplateFormComponent } from './appgrid-template-form.component'
 
 @NgModule({
-    declarations: [AppTemplateFormComponent],
+    declarations: [AppTemplateFormComponent, AppGridValueFieldComponent],
     imports: [ShareModule, ReactiveFormsModule],
     exports: [AppTemplateFormComponent],
 })
---
+++ src/app/appgrid-value-field/appgrid-value-field.component.ts
@@ -0,0 +1,59 @@
+import { GRID_UNITS } from '../types'
+import { FormControl, FormGroup } from '@angular/forms'
+import { Component, Input, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-grid-value-field',
+    template: `
+        <ng-container [formGroup]="formGroup">
+            <mat-form-field>
+                <input type="number" matInput [placeholder]="placeholder" [formControl]="formControl" />
+            </mat-form-field>
+            <mat-form-field>
+                <mat-select [placeholder]="unitPlaceholder" [formControl]="unitFormControl">
+                    <mat-option *ngFor="let unit of unitList" [value]="unit">
+                        {{ unit }}
+                    </mat-option>
+                </mat-select>
+            </mat-form-field>
+        </ng-container>
+    `,
+    styles: [
+        `
+            :host {
+                display: block;
+            }
+
+            mat-form-field {
+                margin-right: 1rem;
+            }
+        `,
+    ],
+})
+export class AppGridValueFieldComponent implements OnInit {
+    @Input()
+    controlName: string
+
+    @Input()
+    unitControlName: string
+
+    @Input()
+    placeholder: string
+
+    @Input()
+    unitPlaceholder: string
+
+    @Input()
+    unitList: typeof GRID_UNITS
+
+    @Input()
+    formGroup: FormGroup
+
+    formControl: FormControl
+    unitFormControl: FormControl
+
+    ngOnInit() {
+        this.formControl = this.formGroup.get(this.controlName) as FormControl
+        this.unitFormControl = this.formGroup.get(this.unitControlName) as FormControl
+    }
+}
