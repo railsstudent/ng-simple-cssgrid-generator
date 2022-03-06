60f03973fb6fad59a1d77eca35045ba5146495a9
Connie Leung <cleung334@gmail.com>
2022-03-06 16:35:35 +0800

    chore: prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,7 @@
 module.exports = {
     printWidth: 120,
-    tabWidth: 4,
-    useTabs: false,
+    tabWidth: 2,
     semi: true,
     singleQuote: true,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: 'avoid',
-    rangeStart: 0,
-    rangeEnd: Infinity,
-    requirePragma: false,
-    insertPragma: false,
-    proseWrap: 'preserve',
+    trailingComma: 'all'
 };
