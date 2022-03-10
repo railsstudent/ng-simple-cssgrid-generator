ea7befa6205036a8f0f9fae468479d85a6d77f3b
Connie Leung <cleung334@gmail.com>
2022-03-10 10:22:19 +0800

    fix: replace grid-gap to gap

--- src/app/appgrid-form/appgrid-form.component.ts
+++ src/app/appgrid-form/appgrid-form.component.ts
@@ -1,6 +1,8 @@
 import { NUM_GAP_LENGTHS, AUTO_FLOW, GAP_UNITS } from '../app.types'
 import { AbstractControl, FormGroup, FormGroupDirective } from '@angular/forms'
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { takeUntil, tap } from 'rxjs/operators'
+import { Subject } from 'rxjs'
 
 @Component({
     selector: 'app-grid-form',
@@ -21,24 +23,52 @@ import { Component, Input, OnInit } from '@angular/core'
         `,
     ],
 })
-export class AppgridFormComponent implements OnInit {
+export class AppgridFormComponent implements OnInit, OnDestroy {
     @Input()
     formGroupName: string
 
     form: FormGroup
 
-    gapUnits = GAP_UNITS
-    gapColUnits = GAP_UNITS
-    gridAutoFlowChoices = AUTO_FLOW
-    gapLengths = NUM_GAP_LENGTHS
+    readonly gapUnits = GAP_UNITS
+    readonly gapColUnits = GAP_UNITS
+    readonly gridAutoFlowChoices = AUTO_FLOW
+    readonly gapLengths = NUM_GAP_LENGTHS
+
+    destroy$ = new Subject<boolean>()
 
     constructor(private rootFormGroup: FormGroupDirective) {}
 
     ngOnInit(): void {
         this.form = this.rootFormGroup.control.get(this.formGroupName) as FormGroup
+
+        this.numGapLengths.valueChanges
+            .pipe(
+                tap((value) => {
+                    const numLengths = value as number
+                    if (numLengths === 1) {
+                        this.gapCol.setValue(0)
+                        this.gapColUnit.setValue('px')
+                    }
+                }),
+                takeUntil(this.destroy$),
+            )
+            .subscribe()
     }
 
     get numGapLengths() {
         return this.form.get('numGapLengths') as AbstractControl
+    }
+
+    get gapCol() {
+        return this.form.get('gapCol') as AbstractControl
+    }
+
+    get gapColUnit() {
+        return this.form.get('gapColUnit') as AbstractControl
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next(true)
+        this.destroy$.complete()
     }
 }
