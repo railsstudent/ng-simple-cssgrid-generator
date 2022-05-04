6e31ebd24f3a9ce5d1bc92cadff5031bdd299a7b
Connie Leung <cleung334@gmail.com>
2022-05-04 23:29:55 +0800

    refactor: css grid start with values

--- src/app/appgrid-generator/appgrid-generator.constants.ts
+++ src/app/appgrid-generator/appgrid-generator.constants.ts
@@ -1,43 +1,57 @@
-import { ControlMapping, GridForm, GridTemplateInfo } from '../app.types'
+import { GridUnitsType, ControlMapping, GridForm, GridTemplateInfo } from '../app.types'
+
+export const GRID_FORM_START_WITH: GridForm = {
+    heightInPixel: 150,
+    numDivs: 20,
+    gridAutoFlow: 'row',
+    numGapLengths: 1,
+    gap: 0,
+    gapUnit: 'px',
+    gapCol: 0,
+    gapColUnit: 'px',
+    gridAutoRowsKeyword: 'auto',
+    gridAutoRowsField: 0,
+    gridAutoRowsUnit: 'px',
+}
 
 export const GRID_CONTROL_NAMES: ControlMapping = {
     heightInPixel: {
-        value: 150,
+        value: GRID_FORM_START_WITH.heightInPixel,
         updateOn: 'blur',
     },
     numDivs: {
-        value: 20,
+        value: GRID_FORM_START_WITH.numDivs,
         updateOn: 'blur',
     },
     gridAutoFlow: {
-        value: 'row',
+        value: GRID_FORM_START_WITH.gridAutoFlow,
     },
     numGapLengths: {
-        value: 1,
+        value: GRID_FORM_START_WITH.numGapLengths,
     },
     gap: {
-        value: 0,
+        value: GRID_FORM_START_WITH.gap,
         updateOn: 'blur',
     },
     gapUnit: {
-        value: 'px',
+        value: GRID_FORM_START_WITH.gapUnit,
     },
     gapCol: {
-        value: 0,
+        value: GRID_FORM_START_WITH.gapCol,
         updateOn: 'blur',
     },
     gapColUnit: {
-        value: 'px',
+        value: GRID_FORM_START_WITH.gapColUnit,
     },
     gridAutoRowsKeyword: {
-        value: 'auto',
+        value: GRID_FORM_START_WITH.gridAutoRowsKeyword,
     },
     gridAutoRowsField: {
-        value: 0,
+        value: GRID_FORM_START_WITH.gridAutoRowsField,
         updateOn: 'blur',
     },
     gridAutoRowsUnit: {
-        value: 'px',
+        value: GRID_FORM_START_WITH.gridAutoRowsUnit,
     },
 }
 
@@ -64,56 +78,42 @@ const DEFAULT_PROPERTIES: ControlMapping = {
     },
 }
 
+export const TEMPLATE_COLUMNS_START_WITH: GridTemplateInfo = {
+    repeat: `${DEFAULT_PROPERTIES.repeat.value}`,
+    numOfTimes: 5,
+    minmax: `${DEFAULT_PROPERTIES.minmax.value}`,
+    minWidth: 10,
+    minUnit: <GridUnitsType>`${DEFAULT_PROPERTIES.minUnit.value}`,
+    maxWidth: +DEFAULT_PROPERTIES.maxWidth.value,
+    maxUnit: <GridUnitsType>`${DEFAULT_PROPERTIES.maxUnit.value}`,
+}
+
 export const GRID_TEMPLATE_COLUMN_CONTROL_NAMES: ControlMapping = {
     ...DEFAULT_PROPERTIES,
     numOfTimes: {
-        value: 5,
+        value: TEMPLATE_COLUMNS_START_WITH.numOfTimes,
         updateOn: 'blur',
     },
     minWidth: {
-        value: 10,
+        value: TEMPLATE_COLUMNS_START_WITH.minWidth,
         updateOn: 'blur',
     },
+}
+
+export const TEMPLATE_ROWS_START_WITH: GridTemplateInfo = {
+    repeat: `${DEFAULT_PROPERTIES.repeat.value}`,
+    numOfTimes: +DEFAULT_PROPERTIES.numOfTimes.value,
+    minmax: `${DEFAULT_PROPERTIES.minmax.value}`,
+    minWidth: 20,
+    minUnit: <GridUnitsType>`${DEFAULT_PROPERTIES.minUnit.value}`,
+    maxWidth: +DEFAULT_PROPERTIES.maxWidth.value,
+    maxUnit: <GridUnitsType>`${DEFAULT_PROPERTIES.maxUnit.value}`,
 }
 
 export const GRID_TEMPLATE_ROW_COLUMN_NAMES: ControlMapping = {
     ...DEFAULT_PROPERTIES,
     minWidth: {
-        value: 20,
+        value: TEMPLATE_ROWS_START_WITH.minWidth,
         updateOn: 'blur',
     },
-}
-
-export const TEMPLATE_COLUMNS_START_WITH: GridTemplateInfo = {
-    repeat: 'true',
-    numOfTimes: 5,
-    minmax: 'true',
-    minWidth: 10,
-    minUnit: 'px',
-    maxWidth: 1,
-    maxUnit: 'fr',
-}
-
-export const TEMPLATE_ROWS_START_WITH: GridTemplateInfo = {
-    repeat: 'true',
-    numOfTimes: 2,
-    minmax: 'true',
-    minWidth: 20,
-    minUnit: 'px',
-    maxWidth: 25,
-    maxUnit: 'px',
-}
-
-export const GRID_FORM_START_WITH: GridForm = {
-    heightInPixel: 150,
-    numDivs: 4,
-    gridAutoFlow: 'row',
-    numGapLengths: 1,
-    gap: 0,
-    gapUnit: 'px',
-    gapCol: 0,
-    gapColUnit: 'px',
-    gridAutoRowsKeyword: 'auto',
-    gridAutoRowsField: 0,
-    gridAutoRowsUnit: 'px',
 }
