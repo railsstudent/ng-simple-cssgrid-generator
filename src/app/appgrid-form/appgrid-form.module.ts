31f6afbc8397454f96304ccf75d9a1bfcdb81b85
Connie Leung <cleung334@gmail.com>
2022-05-14 11:37:12 +0800

    refactor: remove ShareModule from AppGridForm module

--- src/app/appgrid-form/appgrid-form.module.ts
+++ src/app/appgrid-form/appgrid-form.module.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
 import { AppgridAutoRowsModule } from '../appgrid-auto-rows'
 import { AppGridValueFieldModule } from '../appgrid-value-field'
-import { ShareModule } from '../share.module'
 import { AppgridFormComponent } from './appgrid-form.component'
 
 @NgModule({
     declarations: [AppgridFormComponent],
-    imports: [ShareModule, ReactiveFormsModule, AppGridValueFieldModule, AppgridAutoRowsModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        AppGridValueFieldModule,
+        AppgridAutoRowsModule,
+    ],
     exports: [AppgridFormComponent],
 })
 export class AppGridFormModule {}
